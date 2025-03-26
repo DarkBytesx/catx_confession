a6f1312e4225a8a268a4c5df6e199d842f94f0da
HairiRaven <152843094+DarkBytesx@users.noreply.github.com>
2025-03-26 11:11:46 +0800

    Delete catx_confession directory

--- catx_confession/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.ConfessionFee = 1000 -- every confession
-
-Config.ConfessionWebhook = "" -- webhook
-
-Config.ConfessionSociety = "society_admin" -- kung san papasok ung confession fee 
