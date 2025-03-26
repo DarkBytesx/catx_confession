571dc430bd94265dac18218b5bcf83145108c349
HairiRaven <152843094+DarkBytesx@users.noreply.github.com>
2025-03-26 11:14:38 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.ConfessionFee = 1000 -- every confession
 
-Config.ConfessionWebhook = "" -- webhook
+Config.ConfessionWebhook = "" -- webhook san babagsak ung confession
 
 Config.ConfessionSociety = "society_admin" -- kung san papasok ung confession fee 
